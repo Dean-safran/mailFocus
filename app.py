7fc08cdf2fb83823a1feae52e02db02f9ae59f2a
Dean Safran <deansafran@mac.myfiosgateway.com>
2026-07-19 12:08:56 -0400

    completed fake email dashboard

--- app.py
+++ app.py
@@ -4,24 +4,28 @@ app = Flask(__name__)
 
 emails = [
     {
+        "id": 1,
         "sender": "Professor Smith",
         "subject": "Please send your project",
         "priority": 90,
         "status": "Needs Reply"
     },
     {
+        "id": 2,
         "sender": "GitHub",
         "subject": "New login detected",
         "priority": 60,
         "status": "Review"
     },
     {
+        "id": 3,
         "sender": "Clothing Store",
         "subject": "20% off today",
         "priority": 10,
         "status": "No Action"
     },
     {
+        "id": 4,
         "sender": "Polo Ralph",
         "subject": "New sweaters for sale",
         "priority": 10,
@@ -31,8 +35,10 @@ emails = [
 
 @app.route("/")
 def dashboard() :
+    # sort emails
     emails.sort(key=lambda x: x["priority"], reverse=True)
     sorted_emails = emails
+    # pass emails to template
     return render_template('dashboard.html', emails=sorted_emails)
 
 if __name__ == "__main__" :
